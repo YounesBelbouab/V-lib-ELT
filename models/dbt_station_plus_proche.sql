22e5e8e414bb18d57d4c5d812e3c253af34f14a5
Léopold de Terline <leopold.deterline@gmail.com>
2025-05-20 14:10:27 +0200

    ajout des dbt_modèls

---
+++ models/dbt_station_plus_proche.sql
@@ -0,0 +1,56 @@
+WITH base AS (
+  SELECT
+    si.station_id AS station_id,
+    si.name AS station_name,
+    si.address,
+    si.lat,
+    si.lon,
+    si.capacity,
+    ss.is_installed,
+    ss.is_renting,
+    ss.is_returning,
+    ss.num_bikes_available,
+    ss.num_docks_available
+  FROM {{ source('velib_dataset', 'station_information') }} si
+  LEFT JOIN {{ source('velib_dataset', 'station_status') }} ss
+    ON si.station_id = ss.station_id
+),
+
+distances AS (
+  SELECT
+    a.station_id AS station_id,
+    a.name AS station_name,
+    a.lat AS lat_a,
+    a.lon AS lon_a,
+    b.station_id AS closest_station_id,
+    b.name AS closest_station_name,
+    b.lat AS lat_b,
+    b.lon AS lon_b,
+    6371 * ACOS(
+      COS(a.lat * 3.141592653589793 / 180) * COS(b.lat * 3.141592653589793 / 180) *
+      COS((b.lon - a.lon) * 3.141592653589793 / 180) +
+      SIN(a.lat * 3.141592653589793 / 180) * SIN(b.lat * 3.141592653589793 / 180)
+    ) AS distance_km
+  FROM {{ source('velib_dataset', 'station_information') }} a
+  JOIN {{ source('velib_dataset', 'station_information') }} b
+    ON a.station_id != b.station_id
+),
+
+min_distances AS (
+  SELECT
+    station_id,
+    station_name,
+    ARRAY_AGG(STRUCT(closest_station_id, closest_station_name, distance_km)
+              ORDER BY distance_km ASC)[OFFSET(0)] AS nearest
+  FROM distances
+  GROUP BY station_id, station_name
+)
+
+SELECT DISTINCT
+  b.*,
+  m.nearest.closest_station_id,
+  m.nearest.closest_station_name,
+  ROUND(m.nearest.distance_km, 3) AS distance_km_to_closest
+FROM base b
+JOIN min_distances m
+  ON b.station_id = m.station_id
---
+++ target/compiled/velib_project/models/stations_en_travaux.sql
@@ -0,0 +1,38 @@
+
+
+WITH station_status AS (
+    SELECT
+        station_id,
+        is_installed,
+        is_renting,
+        is_returning,
+        last_reported
+    FROM `velib-460309.velib_dataset.station_status`
+),
+
+station_information AS (
+    SELECT
+        station_id,
+        name,
+        address,
+        lat,
+        lon
+    FROM `velib-460309.velib_dataset.station_information`
+)
+
+SELECT
+    info.station_id,
+    info.name,
+    info.address,
+    info.lat,
+    info.lon,
+    status.is_installed,
+    status.is_renting,
+    status.is_returning,
+    status.last_reported
+FROM station_status AS status
+JOIN station_information AS info
+    ON status.station_id = info.station_id
+WHERE status.is_installed = 0
+   OR status.is_renting = 0
+   OR status.is_returning = 0
